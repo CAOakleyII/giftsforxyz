fededf68841112d4414788b9814eb1369f2b3991
Chris Oakley <CAOakleyII@gmail.com>
2015-12-03 02:11:39 -0500

    added an api get request for the gifts back end

--- server/publiciations.js
+++ server/publiciations.js
@@ -1,5 +1,28 @@
 Meteor.publish('gifts', function(){
-    return Gifts.find();
+  var self = this;
+  var gifts = [];
+
+  try {
+    console.log('api call');
+    var response = HTTP.get('http://159.203.74.143:2456/items');
+
+    if (response.statusCode  !== 200) {
+      // Error retrieving gifts from API.
+      console.log("API responded with a status code of " + response.statusCode  +
+      " when attempting to retrieve gifts.");
+    } else {
+      gifts = response.data;
+    }
+
+    _.each(gifts, function(gift) {
+      self.added('gifts', gift._id, gift);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  self.ready();
 });
 
 Meteor.publish('users', function(){
